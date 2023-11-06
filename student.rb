7ec92a868249ba467af12fa521d85826db631464
“sadaf <sadaf.batekzai54321@gmial.com>
2023-11-06 09:35:55 +0430

    add new files

--- student.rb
+++ student.rb
@@ -1,7 +1,9 @@
 require './person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
