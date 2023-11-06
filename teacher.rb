8aa2722924c890f6575184f8981ca96947647b81
Sadaf-Daneshgar <sadaf.barekzai54321@gmail.com>
2023-11-06 09:29:27 +0430

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,9 @@
 require './person'
 class Teacher < Person
+  attr_accessor :specialization
+  
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
