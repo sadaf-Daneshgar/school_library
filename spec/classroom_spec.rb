6cfbbea89bb1b0dc093d9791bc1a6deec82830d6
MohammadYaser <yasersafi19@outlook.com>
2023-11-14 22:48:40 +0430

    add unit test for classroom

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,36 @@
+# classroom_spec.rb
+
+require_relative '../classroom'
+require_relative '../student'
+
+RSpec.describe Classroom do
+  describe '#initialize' do
+    it 'creates a new classroom with a label' do
+      classroom = Classroom.new('Math Room')
+      expect(classroom.label).to eq('Math Room')
+    end
+
+    it 'initializes with an empty list of students' do
+      classroom = Classroom.new('English Room')
+      expect(classroom.students).to be_empty
+    end
+  end
+
+  describe '#label' do
+    it 'can change the label of the classroom' do
+      classroom = Classroom.new('Physics Room')
+      classroom.label = 'Chemistry Room'
+      expect(classroom.label).to eq('Chemistry Room')
+    end
+  end
+
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      classroom = Classroom.new('History Room')
+      student = instance_double('Student', name: 'John', classroom: nil)
+      allow(student).to receive(:classroom=)
+      classroom.add_student(student)
+      expect(classroom.students).to include(student)
+    end
+  end
+end
