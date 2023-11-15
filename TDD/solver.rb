0976abf36192cf0e8135979160c42506b1663558
“sadaf <sadaf.batekzai54321@gmial.com>
2023-11-15 13:51:11 +0430

    create method of factorial and test for that

--- TDD/solver.rb
+++ TDD/solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  def factorial(numb)
+    if numb.zero?
+      1
+    elsif numb.positive?
+      numb * factorial(numb - 1)
+    else
+      raise 'an exception'
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,18 @@
 require 'rspec'
 require_relative '../TDD/solver'
 
-describe Solver do
+RSpec.describe Solver do
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
+      expect(subject.factorial(0)).to eq(1)
+    end
+
+    it 'returns 120 when given 5' do
+      expect(subject.factorial(5)).to eq(120)
+    end
+
+    it 'raises an exception when given a negative number' do
+      expect { subject.factorial(-1) }.to raise_error(RuntimeError)
+    end
+  end
 end
