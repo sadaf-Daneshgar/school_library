e9faa0a584e0b91435437cafa3eaad60e2c00781
MohammadYaser <yasersafi19@outlook.com>
2023-11-15 14:43:12 +0430

    update solver.rb file

--- TDD/solver.rb
+++ TDD/solver.rb
@@ -12,4 +12,14 @@ class Solver
   def reverse(word)
     word.reverse
   end
+  if (numb % 3).zero? && (numb % 5).zero?
+    'fizzbuzz'
+  elsif (numb % 3).zero?
+    'fizz'
+  elsif (numb % 5).zero?
+    'buzz'
+  else
+    numb.to_s
+  end
+  end  
 end
